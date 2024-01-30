5e21514ea9770599c9977ae6280822b267df6f49
Fioaq <108437802+Fioaq@users.noreply.github.com>
2024-01-29 22:45:25 -0300

    mapbox map

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,5 +1,6 @@
 import Map from "@/components/Map/page";
 import NavBar from "@/components/NavBar/page";
+import { Box } from "@mui/material";
 import { Fragment } from "react";
 
 export default function Home() {
--- client/src/components/Map/page.jsx
+++ client/src/components/Map/page.jsx
@@ -1,58 +1,44 @@
 "use client"
-import { Loader } from "@googlemaps/js-api-loader";
-import React, { useEffect, useRef } from "react";
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import styles from './page.module.css';
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY;
 
 const Map = () => {
-    //Almacena valores mutables que no provocan una nueva renderizacion
-    const mapRef = useRef(null);
+    const mapContainer = useRef(null);
+    const map = useRef(null); // useRef previene que el mapa se recargue cada vez que el usuario interactue
+    const [lng, setLng] = useState(-55.86);
+    const [lat, setLat] = useState(-27.34);
+    const [zoom, setZoom] = useState(13);
 
     useEffect(() => {
-        //Funcion para iniciar el mapa
-        const initMap = async () => {
-            const loader = new Loader({//Carga la API de Maps utilizando la clave de API
-                apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-                version: "weekly"//Se encarga de actualizar la API de Maps semanalmente
-            });
-            //Trae la libreria del mapa y del marcador
-            const { Map } = await loader.importLibrary("maps");
-            const { Marker } = await loader.importLibrary("marker");
-            //Determina la posicion central del mapa
-            const position = {
-                lat: -27.331406,
-                lng: -55.866329
-            };
-            //Asigna el centro, el zoom y el id de mapa generado
-            const mapOptions = {
-                center: position,
-                zoom: 15,
-                mapId: process.env.NEXT_PUBLIC_MAP_ID
-            };
-            //Configura el mapa teniendo en cuenta las opciones anteriores
-            const map = new Map(mapRef.current, mapOptions);
+        // Inicia el mapa solo una vez
+        if (map.current) return;
 
-            /*
-            const svgMarker = {
-                path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-                fillColor: "blue",
-                fillOpacity: 0.6,
-                strokeWeight: 0,
-                rotation: 0,
-                scale: 2,
-                anchor: new google.maps.Point(0, 20),
-            };
+        // Asigna los valores al mapa
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current, // Le dice a MapBox que renderice el mapa dentro de un elemento DOM especifico
+            style: 'mapbox://styles/mapbox/streets-v12', // Estilo que va a tener el mapa
+            center: [lng, lat],
+            zoom: zoom
+        });
 
-            //Pone el marcador
-            const marker = new Marker({
-                map: map,
-                position: position,
-                //icon: svgMarker
-            });*/
-        }
-        //LLama la funcion para iniciar el mapa
-        initMap();
-    }, [])
+        // Guarda la nueva latitud, longitud y zoom cuando el usuario interactua con el mapa
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4)); // toFixed para modificar la cantidad de digitos que tendran ej. 22.54567 => 22.54  
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
     return (
-        <div style={{ height: "92vh", width: "100vw" }} ref={mapRef} />
+        <div>
+            <div className={styles.sidebar}>
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} style={{ height: "100vh" }} />
+        </div>
     )
 };
 
