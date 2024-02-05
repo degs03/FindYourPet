96452ce67503c909577cd6ca2a1b682830c130fc
DarioGimenez03 <gimenezdario1617@gmail.com>
2024-02-05 14:13:02 -0300

    All posts section

--- client/src/app/posts/page.jsx
+++ client/src/app/posts/page.jsx
@@ -1,10 +1,63 @@
-import { Fragment } from "react";
 
+'use client'
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import {  Card, CardActions, CardContent, CardMedia, Container, Grid, ImageList, ImageListItem, Typography } from '@mui/material';
+import ShareIcon from '@mui/icons-material/Share';
+import { Fragment } from 'react';
 export default function Posts() {
+    const [posts, setPosts] = useState([]);
+    const PostsInfo = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/post/all`);
+            const result = await response.data;
+            console.log(result);
+            setPosts(result);
+            console.log(posts)
+        } catch (error) {
+            console.log({ error: error })
+        }
+    }
+    useEffect(() => {
+        PostsInfo();
+    }, []);
 
     return (
         <Fragment>
-            <h1>Posts</h1>
+            <Container sx={{ py: 8 }} maxWidth="md">
+                {/* End hero unit */}
+                <Grid container spacing={4}>
+                    {posts.map((card) => (
+                        <Grid item key={card} xs={12} sm={12} >
+                            <Card
+                                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                            >
+                                <CardMedia
+                                    component="div"
+                                    sx={{
+                                        // 16:9
+                                        pt: '56.25%',
+                                    }}
+                                    image={card.image}  
+                                />
+
+                                <CardContent sx={{ flexGrow: 1 }}>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {card.title}
+                                    </Typography>
+                                    <Typography sx={{ overflowY: "scroll" }}>
+                                        {card.description}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <ShareIcon />
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
         </Fragment>
     );
+
 }
